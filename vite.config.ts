a5554cac8459903b6d8233fb71b8411c6028a013
Miroslav Peterka <wiki@atlas.cz>
2022-07-27 19:56:37 +0200

    change port to 8080, auto open

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    host: 'localhost',
+    port: 8080,
+    strictPort: true,
+    open: true,
+  },
 })
